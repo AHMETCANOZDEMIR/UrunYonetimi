d918b48adabd5706ec678c23a788d3c6e5043bbc
AHMETCANOZDEMIR <126383568+AHMETCANOZDEMIR@users.noreply.github.com>
2023-06-25 13:54:13 +0300

    UrunYonetimi
    Urun Yonetimi panel projesi

---
+++ UrunYonetim6584-master/UrunYonetim6584.Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using UrunYonetim6584.Entities;
+
+namespace UrunYonetim6584.Data
+{
+    public class DatabaseContext : DbContext // DbContext class ı nuget dan yüklediğimiz entityframework den geliyor
+    {
+        public DbSet<Category> Categories { get; set; } // veritabanı tablolarımızı temsil eden dbset ler
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DatabaseContext() : base("name=UrunYonetimiDb")
+        {
+            Database.SetInitializer(new DbInitializer()); // DbInitializer classımızı bu şekilde kurucu metotta çağırıyoruz çalışması için
+        }
+        // model değişikliği durumlarında veritabanını silme gibi sorunlar yaşamamak için migration sistemi kullanılır!
+        // Migration kullanmak için Tools menüsünden Nuget package manager > package manager console menüsüne tıklayıp komut çalıştırma alanını aşağıya getiriyoruz.
+        // Bu alanda default project kısmından Data proje katmanını seçiyoruz
+        // Komut yazma alanına enable-migrations yazıp enter a basıyoruz
+        // Migration sistemi aktif olduktan sonra artık db yi silmemize gerek yok
+        // update-database komutunu yazıp enter diyerek güncellemeleri db ye yansıtabiliriz
+        // köklü bir değişiklik yaptığımızda ise add-migration migrationismi şeklinde komut yazıp enter deyip tekrar update-database komutuyla daha büyük değişiklikleri de db ye yansıtabiliriz.
+    }
+}
---
+++ UrunYonetim6584-master/UrunYonetim6584.WinFormUI/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UrunYonetim6584.WinFormUI
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void kategoriYönetimiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KategoriYonetimi kategoriYonetimi = new KategoriYonetimi();
+            kategoriYonetimi.ShowDialog();
+        }
+
+        private void ürünYönetimiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UrunYonetimi urunYonetimi = new UrunYonetimi();
+            urunYonetimi.ShowDialog();
+        }
+
+        private void kullanıcıYönetimiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KullaniciYonetimi kullaniciYonetimi = new KullaniciYonetimi();
+            kullaniciYonetimi.ShowDialog();
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit(); // form kapatıldığında uygulamadan çık
+        }
+    }
+}
